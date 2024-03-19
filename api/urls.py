e69fadc5af96fe3f08ac5ff202ff4696c17f5142
bouaik9 <ayoub.bouaik.22@gmail.com>
2024-03-19 22:19:01 +0000

    we can get id of other variables now

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,15 @@ from .views import *
 urlpatterns = [
     path('book/', BookView.as_view(), name='book'),
     path('author/', AuthorView.as_view(), name='author'),
+    path('author/<str:author>/', AuthorViewFind.as_view({'get': 'retrieve'}), name='author-find'),
+
     path('publisher/', PublisherView.as_view(), name='publisher'),
-    path('category/', CategoryView.as_view(), name='category'),
+    path('publisher/<str:publisher>/', PublisherViewFind.as_view({'get': 'retrieve'}), name='author-find'),
+
+
+    path('category', CategoryView.as_view(), name='category'),
+    path('category/<str:category>/', CategoryViewFind.as_view({'get': 'retrieve'}), name='author-find'),
+
 
 ]
+
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import views
 from .serializers import *
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework import status
 from rest_framework.response import Response
 
@@ -12,58 +12,93 @@ class BookView(generics.ListCreateAPIView, generics.DestroyAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
 
+#----------------------------------------------------------------------------------------------------------------------
+
 class AuthorView(generics.ListCreateAPIView, generics.DestroyAPIView):
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
     
     def create(self, request, *args, **kwargs):
-        # Initialize an empty list to store serializer instances
         serializer_instances = []
 
-        # Iterate over each author object in the request data list
         for author_data in request.data:
-            # Pass each author object to the serializer
             serializer = self.get_serializer(data=author_data)
-            # Validate the serializer
             serializer.is_valid(raise_exception=True)
-            # Save the serializer instance in the list
             serializer_instances.append(serializer)
-            # Perform creation for each author object
             self.perform_create(serializer)
-
-        # Get the success headers for the last serializer instance
         headers = self.get_success_headers(serializer_instances[-1].data)
         
-        # Return a response with the data of all created authors
         return Response([serializer.data for serializer in serializer_instances], status=status.HTTP_201_CREATED, headers=headers)
+    
+
+class AuthorViewFind(viewsets.ViewSet):
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        author = kwargs.get('author')
+        author = author.replace("+", " ")  # Corrected line
+        instance = Author.objects.filter(name=author).first()
+        if instance:
+            ser = AuthorSerializer(instance)
+            return Response({"id":ser.data['id']})
+        else:
+            return Response({"message": "Author not found"}, status=404)
+
+#----------------------------------------------------------------------------------------------------------------------
+
 
 class CategoryView(generics.ListCreateAPIView, generics.DestroyAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    def create(self, request, *args, **kwargs):
+        serializer_instances = []
+
+        for Category_data in request.data:
+            serializer = self.get_serializer(data=Category_data)
+            serializer.is_valid(raise_exception=True)
+            serializer_instances.append(serializer)
+            self.perform_create(serializer)
+        headers = self.get_success_headers(serializer_instances[-1].data)
+        
+        return Response([serializer.data for serializer in serializer_instances], status=status.HTTP_201_CREATED, headers=headers)
+    
+    
+class CategoryViewFind(viewsets.ViewSet):
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        category = kwargs.get('category')
+        category = category.replace("+", " ")  # Corrected line
+        instance = Category.objects.filter(name=category).first()
+        if instance:
+            ser = CategorySerializer(instance)
+            return Response({"id":ser.data['id']})
+        else:
+            return Response({"message": "Category not found"}, status=404)
+
+
+#----------------------------------------------------------------------------------------------------------------------
 
 class PublisherView(generics.ListCreateAPIView, generics.DestroyAPIView):
     serializer_class = PublisherSerializer
     queryset = Publisher.objects.all()
     def create(self, request, *args, **kwargs):
-        # Initialize an empty list to store serializer instances
         serializer_instances = []
-
-        # Iterate over each author object in the request data list
         for author_data in request.data:
-            # Pass each author object to the serializer
             serializer = self.get_serializer(data=author_data)
-            # Validate the serializer
             serializer.is_valid(raise_exception=True)
-            # Save the serializer instance in the list
             serializer_instances.append(serializer)
-            # Perform creation for each author object
             self.perform_create(serializer)
-
-        # Get the success headers for the last serializer instance
         headers = self.get_success_headers(serializer_instances[-1].data)
-        
-        # Return a response with the data of all created authors
         return Response([serializer.data for serializer in serializer_instances], status=status.HTTP_201_CREATED, headers=headers)
+    
+class PublisherViewFind(viewsets.ViewSet):
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        publisher = kwargs.get('publisher')
+        print(publisher)
+        publisher = publisher.replace("+", " ")  # Corrected line
+        instance = Publisher.objects.filter(name=publisher).first()
+        if instance:
+            ser = PublisherSerializer(instance)
+            return Response({"id":ser.data['id']})
+        else:
+            return Response({"message": "publisher not found"}, status=404)
 
 
 
